fe26c6bc42c674ca46bf57b924ecefcb52542a7f
Martin-wwh <1454565178@qq.com>
2019-11-05 02:02:43 +0800

    commit the sum3 question. the method is sort+two point

---
+++ simple/sum3.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env  python
+# -*- coding:utf-8 -*-
+
+# @Create_time     :2019/11/5 1:18
+# @Author          :wuweihong6
+# @e-mail          :1454565178@qq.com
+# @File            :sum3.py
+# @software        :PyCharm
+# @description     :
+
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        n = len(nums)
+        res = []
+        if (not nums or n < 3):
+            return []
+        nums.sort()
+        res = []
+        for i in range(n):
+            if (nums[i] > 0):
+                return res
+            if (i > 0 and nums[i] == nums[i - 1]):
+                continue
+            L = i + 1
+            R = n - 1
+            while (L < R):
+                if (nums[i] + nums[L] + nums[R] == 0):
+                    res.append([nums[i], nums[L], nums[R]])
+                    while (L < R and nums[L] == nums[L + 1]):
+                        L = L + 1
+                    while (L < R and nums[R] == nums[R - 1]):
+                        R = R - 1
+                    L = L + 1
+                    R = R - 1
+                elif (nums[i] + nums[L] + nums[R] > 0):
+                    R = R - 1
+                else:
+                    L = L + 1
+        return res
+
